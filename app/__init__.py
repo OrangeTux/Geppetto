26e1bc760f33b649309de941f1c8d3b405df3dc4
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-10-07 00:38:27 +0200

    #15 Made api authentication Unicode proof. Implemented test_authentication endpoint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 import os
 import base64
-from flask import Flask
-from flask.ext.login import LoginManager
+import binascii
+from flask import Flask, Response
+from flask.ext.login import LoginManager, login_required
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from app.api import gpio
@@ -12,7 +13,6 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 app.register_blueprint(gpio, url_prefix='/gpio')
-app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///%s/data/geppettov.db' % _cur_path
 
@@ -44,15 +44,27 @@ def load_user_from_request(request):
 
     """
     key = request.headers.get('Authorization')
+
     if key:
         key = key.replace('Basic ', '', 1)
         try:
             key = base64.b64decode(key)
-        except TypeError:
-            pass
+        except (TypeError, binascii.Error):
+            return None
 
-        user = User.query.filter_by(api_key=key).first()
+        try:
+            user = User.query.filter_by(api_key=key.decode('utf-8')).first()
+        except UnicodeError:
+            return None
+
         if user:
             return user
 
     return None
+
+
+@app.route('/test_authentication')
+@login_required
+def test_authentication():
+    """ Endpoint to test authentication. """
+    return Response(status=200)
